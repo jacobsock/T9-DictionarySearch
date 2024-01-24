2eeb2578c4a2b1c0c18b39bc0e88ff249555b0ee
Jacob Sock <jacobmsock@gmail.com>
2024-01-24 12:30:34 -0500

    Initial Commit

---
+++ T9-DictionarySearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T9-DictionarySearch
+//
+//  Created by Jacob M Sock  on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T9-DictionarySearch/T9_DictionarySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  T9_DictionarySearchApp.swift
+//  T9-DictionarySearch
+//
+//  Created by Jacob M Sock  on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct T9_DictionarySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
